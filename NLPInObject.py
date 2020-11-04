054d8be1721273b40a8c0759ccd40ba5ad0a3ea6
shiran1992s@gmail.com <47076788+shiran1992s@users.noreply.github.com>
2020-11-04 20:14:31 +0200

    After Creating HighLight Functionality

--- NLPInObject.py
+++ NLPInObject.py
@@ -4,20 +4,33 @@ class NLPInObject:
         self.text = text_object
         self.expressions = expressions
         self.name = "TEMP"
+        self.words_to_mark = list()
 
         # make dictionary of label: expressions from the input text
-    def expressionTxtToDict(self):
+    def expression_txt_to_dict(self):
         exps = self.expressions
         # parsing
         words = exps.replace("\"", "")
         words = words.split("),")
-        expressions = dict()
-
+        global expressions_dict
+        expressions_dict = dict()
         for i in range(len(words)):
             if i < len(words) - 1:
                 # adding ) to end of each expressions
                 words[i] += ")"
             # parsing
             temp = words[i].split(":")
-            expressions[temp[0]] = temp[1]
-        print(expressions)
+            expressions_dict[temp[0]] = list(temp[1].split())
+        print(expressions_dict)
+
+    def get_words_to_mark(self):
+        for key in expressions_dict:
+            #print(key)
+            for value in expressions_dict[key]:
+                value = value.replace(")", "")
+                value = value.replace("(", "")
+                value = value.replace(",", "")
+                value = value.lower()
+                print(value)
+                self.words_to_mark.append(value)
+
